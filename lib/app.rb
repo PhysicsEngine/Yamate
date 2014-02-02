31fc7a2349ecea883335a3e0922a2cb60c59fbcd
Lewuathe <lewuathe@me.com>
2014-02-02 11:16:06 +0900

    Change layout

--- lib/app.rb
+++ lib/app.rb
@@ -3,8 +3,10 @@ require 'haml'
 
 module Yamate
   class App < Sinatra::Base
-    get "/" do
-      haml :index
+    ['/', %r{^/\d+/?$}].each do |path|
+      get path do
+        send_file File.join(settings.public_folder, 'index.html')
+      end
     end
   end
 end
