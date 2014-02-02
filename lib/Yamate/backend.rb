1770979e28eccb8dc4df1f25e1f517cefa298016
Lewuathe <lewuathe@me.com>
2014-02-02 19:44:35 +0900

    Convert position scale

--- lib/Yamate/backend.rb
+++ lib/Yamate/backend.rb
@@ -2,6 +2,7 @@ require 'faye/websocket'
 require 'json'
 
 module Yamate
+
   class Train
     @@radius = 1
     
@@ -12,10 +13,40 @@ module Yamate
       @theta = theta
     end
 
+    def which_quadrant()
+      remain = @theta % (2*Math::PI)
+      if remain >= (7*Math::PI)/4 || remain < (Math::PI)/4 then
+        return 0
+      elsif (Math::PI)/4 <= remain && remain < (3*Math::PI)/4 then
+        return 1 
+      elsif (3*Math::PI)/4 <= remain && remain < (5*Math::PI)/4 then
+        return 2
+      else
+        return 3
+      end
+    end
+
+    def update()
+      if self.which_quadrant() == 0 then
+        @x = @@radius
+        @y = @@radius * Math.tan(@theta)
+      elsif self.which_quadrant() == 1 then
+        @x = @@radius / Math.tan(@theta)
+        @y = @@radius
+      elsif self.which_quadrant() == 2 then
+        @x = -@@radius
+        @y = -@@radius * Math.tan(theta)
+      else
+        @x = -@@radius / Math.tan(@theta)
+        @y = -@@radius
+      end
+    end
+        
     def move()
-      @theta += 0.01
-      @x = @@radius * Math.cos(@theta)
-      @y = @@radius * Math.sin(@theta)
+      @theta += 0.04
+      self.update()
+      #@x = @@radius * Math.cos(@theta)
+      #@y = @@radius * Math.sin(@theta)
     end
 
     def get_position()
@@ -29,9 +60,10 @@ module Yamate
       @app = app
       @clients = []
       @trains  = []
-      init_theta = Random.new()
+      rand_theta = Random.new()
       for i in 0..10 do
-        @trains.push(Train.new(i, init_theta.rand))
+        init_theta = rand_theta.rand
+        @trains.push(Train.new(i, init_theta))
       end
     end
 
@@ -47,7 +79,7 @@ module Yamate
       end
       
       puts data.to_json
-      sleep 2
+      sleep 1
       @clients.each do |client|
         client.send(data.to_json)
       end
