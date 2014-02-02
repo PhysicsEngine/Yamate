4c32cd49c603e7ca9c9ede9846bc2d06516997d5
Lewuathe <lewuathe@me.com>
2014-02-02 17:09:19 +0900

    Add faraday

--- lib/Yamate.rb
+++ lib/Yamate.rb
@@ -1,5 +1,16 @@
 require "Yamate/version"
+require "faraday"
 
 module Yamate
   # Your code goes here...
+  
+  class YamanoteAPIClient
+    def initialize(host)
+      @host = host
+    end
+
+    def get_trains_data(params)
+      
+    end
+  end
 end
