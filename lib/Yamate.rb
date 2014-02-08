fc95a45e623c4c2b37d7d3ba0429716b65431263
Lewuathe <lewuathe@me.com>
2014-02-08 15:32:51 +0900

    Complement an interval

--- lib/Yamate.rb
+++ lib/Yamate.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require "Yamate/version"
 require "faraday"
 require "json"
@@ -6,7 +7,7 @@ require "json"
 
 module Yamate
   # Your code goes here...
-  
+      
   class APIClient
     @@entry_point = "https://api.odpt.org"
     def initialize(consumer_key)
@@ -24,4 +25,129 @@ module Yamate
       return JSON.parse(response.body)
     end
   end
+
+  class TrainState
+    attr_reader :from_station_name, :to_station_name, :progress, :line_name
+    
+    def initialize(from_station_name, to_station_name, progress, line_name)
+      @from_station_name = from_station_name
+      @to_station_name   = to_station_name
+      @progress          = progress
+      @line_name         = line_name
+
+    end
+
+    def estimate_next_step(theta, station_rad, step)
+      if @to_station_name == nil then
+        interval = 0.0
+      else
+        interval = station_rad[@to_station_name].to_f - station_rad[@from_station_name].to_f
+      end
+      return theta + interval * 0.007
+    end
+  end
+
+  class Train
+
+    @@line_name = {
+      "外回" => "outside_line",
+      "内回" => "inside_line"
+    }
+
+    @@station_rad = {
+      "秋葉原" => 0.05,
+      "神田"   => 0.3,
+      "東京"   => 0.5,
+      "有楽町" => 0.7,
+      "新橋"   => 0.9,
+      "浜松町" => 1.1,
+      "田町"   => 1.32,
+      "品川"   => 1.58,
+      "大崎"   => 1.86,
+      "五反田" => 2.07,
+      "目黒"   => 2.24,
+      "恵比寿" => 2.38,
+      "渋谷"   => 2.55,
+      "原宿"   => 2.75,
+      "代々木" => 3.0,
+      "新宿"   => 3.26,
+      "新大久保" => 3.5,
+      "高田馬場" => 3.7,
+      "目白" => 3.8,
+      "池袋" => 4.00,
+      "大塚" => 4.125,
+      "巣鴨" => 4.36,
+      "駒込" => 4.6,
+      "田端" => 4.87,
+      "西日暮里" => 5.1,
+      "日暮里" => 5.3,
+      "鶯谷" => 5.63,
+      "上野" => 5.83,
+      "御徒町" => 6.05
+    }
+
+    @@radius = 1
+    
+    def initialize(id, from_station_name, to_station_name, progress, line_name)
+      @id    = id
+      @x     = 0
+      @y     = 0
+      @line_name = @@line_name[line_name]
+      @step  = 0
+
+      @pre_state = TrainState.new(from_station_name, to_station_name, progress, line_name)
+      
+      if to_station_name == nil then
+        dist = 0.0
+      else
+        dist = (@@station_rad[to_station_name].to_f - @@station_rad[from_station_name].to_f) * progress
+      end
+      
+      @theta = @@station_rad[from_station_name] + dist
+    end
+
+    def estimate()
+      @theta = @pre_state.estimate_next_step(@theta, @@station_rad, @step)
+      @step += 1
+    end
+
+    def which_quadrant()
+      remain = @theta % (2*Math::PI)
+      if remain >= (7*Math::PI)/4 || remain < (Math::PI)/4 then
+        return 0
+      elsif (Math::PI)/4 <= remain && remain < (3*Math::PI)/4 then
+        return 1 
+      elsif (3*Math::PI)/4 <= remain && remain < (5*Math::PI)/4 then
+        return 2
+      else
+        return 3
+      end
+    end
+
+    def update()
+      if self.which_quadrant == 0 then
+        @x = @@radius
+        @y = @@radius * Math.tan(@theta)
+      elsif self.which_quadrant == 1 then
+        @x = @@radius / Math.tan(@theta)
+        @y = @@radius
+      elsif self.which_quadrant == 2 then
+        @x = -@@radius
+        @y = -@@radius * Math.tan(@theta)
+      else
+        @x = -@@radius / Math.tan(@theta)
+        @y = -@@radius
+      end
+    end
+        
+    def move()
+      @theta += 0.04
+      self.update()
+    end
+
+    def get_position()
+      return {:id => @id, :x => @x, :y => @y, :line_name => @line_name }
+    end
+  end
+  
 end
--- lib/Yamate/backend.rb
+++ lib/Yamate/backend.rb
@@ -6,101 +6,12 @@ require 'yaml'
 
 module Yamate
 
-  class Train
-    @@line_name = {
-      "外回" => "outside_line",
-      "内回" => "inside_line"
-    }
-    
-    @@station_rad = {
-      "秋葉原" => 0.05,
-      "神田"   => 0.3,
-      "東京"   => 0.5,
-      "有楽町" => 0.7,
-      "新橋"   => 0.9,
-      "浜松町" => 1.1,
-      "田町"   => 1.32,
-      "品川"   => 1.58,
-      "大崎"   => 1.86,
-      "五反田" => 2.07,
-      "目黒"   => 2.24,
-      "恵比寿" => 2.38,
-      "渋谷"   => 2.55,
-      "原宿"   => 2.75,
-      "代々木" => 3.0,
-      "新宿"   => 3.26,
-      "新大久保" => 3.5,
-      "高田馬場" => 3.7,
-      "目白" => 3.8,
-      "池袋" => 3.99,
-      "大塚" => 4.125,
-      "巣鴨" => 4.36,
-      "駒込" => 4.6,
-      "田端" => 4.87,
-      "西日暮里" => 5.1,
-      "日暮里" => 5.3,
-      "鶯谷" => 5.63,
-      "上野" => 5.83,
-      "御徒町" => 6.05
-    }
-    
-    @@radius = 1
-    
-    def initialize(id, station_name, progress, line_name)
-      @id    = id
-      @x     = 0
-      @y     = 0
-      @line_name = @@line_name[line_name]
-      if @line_name == "外回" then
-        @theta = @@station_rad[station_name] + progress*0.1
-      else
-        @theta = @@station_rad[station_name] - progress*0.1
-      end
-    end
-
-    def which_quadrant()
-      remain = @theta % (2*Math::PI)
-      if remain >= (7*Math::PI)/4 || remain < (Math::PI)/4 then
-        return 0
-      elsif (Math::PI)/4 <= remain && remain < (3*Math::PI)/4 then
-        return 1 
-      elsif (3*Math::PI)/4 <= remain && remain < (5*Math::PI)/4 then
-        return 2
-      else
-        return 3
-      end
-    end
-
-    def update()
-      if self.which_quadrant == 0 then
-        @x = @@radius
-        @y = @@radius * Math.tan(@theta)
-      elsif self.which_quadrant == 1 then
-        @x = @@radius / Math.tan(@theta)
-        @y = @@radius
-      elsif self.which_quadrant == 2 then
-        @x = -@@radius
-        @y = -@@radius * Math.tan(@theta)
-      else
-        @x = -@@radius / Math.tan(@theta)
-        @y = -@@radius
-      end
-    end
-        
-    def move()
-      @theta += 0.04
-      self.update()
-    end
-
-    def get_position()
-      return {:id => @id, :x => @x, :y => @y, :line_name => @line_name }
-    end
-  end
-  
   class Backend
     KEEPALIVE_TIME = 15
+
     
     def initialize(app)
+      @step = 0
       @app = app
       @clients = []
 
@@ -111,9 +22,6 @@ module Yamate
         @api_client = Yamate::APIClient.new(ENV["CONSUMER_KEY"])
       end
 
-      ## TODO: Random initialization is not accurate for train position
-      ##       it should be got API data
-      #@api_client = Yamate::APIClient.new(yamate_config["consumer_key"])
       self.update_train_data
     end
 
@@ -134,22 +42,40 @@ module Yamate
         progress          = train["odpt:progress"]
         train_id          = train["@id"]
         from_station_name = train["odpt:fromStationName"]
-        @trains.push(Train.new(train_id, from_station_name, progress, line_name))
+        to_station_name   = train["odpt:toStationName"]
+        @trains.push(Yamate::Train.new(train_id, from_station_name, to_station_name, progress, line_name))
       end
       @trains.each do |train|
         train.update
       end
     end
+
+    def estimate_train_data()
+      @trains.each do |train|
+        train.estimate
+        train.update
+      end
+
+    end
     
     def routine()
-      self.update_train_data
+      if @step % 60 == 0 then
+        puts "************ UPDATE *************"
+        self.update_train_data
+        @step = 0
+      else
+        puts "------------ ESTIMATE ------------"
+        self.estimate_train_data
+      end
+      @step += 1
 
+      # Preparing seding data
       data = {}
       data[:trains] = []
       @trains.each do |train|
         data[:trains].push(train.get_position)
       end
-      sleep 3
+      sleep 1
       @clients.each do |client|
         client.send(data.to_json)
       end
